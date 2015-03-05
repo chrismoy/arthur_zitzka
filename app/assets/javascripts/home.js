9594fdd4db081c167636725bb7a067152bdb8dba
Chris Moy <moyboy530@gmail.com>
2015-03-04 23:08:56 -0600

    Add scroll down indicator

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,11 @@
 $(document).ready( function() {
   $('.carousel').carousel({
     interval: 7000
-  })
+  });
+
+  $(document).scroll(function() {
+    if($('body').scrollTop() > 50) {
+      $('#scroll-helper').fadeOut(500);
+    }
+  });
 });
